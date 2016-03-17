d5d78261399a95252b851829d237756edda7a8ef
ryant26 <ryant26@gmail.com>
2016-03-17 16:41:10 -0600

    task 1 almost done

--- src/task1.scala
+++ src/task1.scala
@@ -1,32 +1,4 @@
-import scala.io.Source
+val text = sc.textFile("data_input")
+val result = text.map(line => (line.split("\t")(0), 1)).reduceByKey(_ + _).reduce((a,b) => if (a._2 < b._2) b else a)._1
 
-val nodes = scala.collection.mutable.HashMap.empty[Int,Int]
-
-Source.fromFile("data_input").getLines().foreach { line => 
-	println(s"line: $line")
-	var tokens = line.split(" +")
-
-	println(s"tokens 0: $tokens(0), tokens 1: $tokens(1)")
-	if (tokens.length > 1){
-		var fromNode = tokens(0).toInt
-		var toNode = tokens(1).toInt
-
-		if (nodes.contains(fromNode)){
-			nodes(fromNode) = nodes(fromNode) + 1
-		} else {
-			nodes(fromNode) = 1
-		}	
-	}
-}
-
-var maxValue = -1
-var maxNode = -1
-
-nodes.keys.foreach { node =>
-	if (nodes(node) > maxValue){
-		maxValue = nodes(node)
-		maxNode = nodes(node)
-	}
-}
-
-println(s"maxNode: $maxNode, with value: $maxValue")
+// I believe I selected the wrong neighbor
