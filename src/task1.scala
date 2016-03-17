9948265890eaaac157b5afc9ef0280fc69a312e8
ryant26 <ryant26@gmail.com>
2016-03-17 15:18:49 -0600

    tried task 1

--- src/task1.scala
+++ src/task1.scala
@@ -1,4 +1,32 @@
 import scala.io.Source
-Source.fromPath("myfile.txt").getLines().foreach {
-	line => println(line)
+
+val nodes = scala.collection.mutable.HashMap.empty[Int,Int]
+
+Source.fromFile("data_input").getLines().foreach { line => 
+	println(s"line: $line")
+	var tokens = line.split(" +")
+
+	println(s"tokens 0: $tokens(0), tokens 1: $tokens(1)")
+	if (tokens.length > 1){
+		var fromNode = tokens(0).toInt
+		var toNode = tokens(1).toInt
+
+		if (nodes.contains(fromNode)){
+			nodes(fromNode) = nodes(fromNode) + 1
+		} else {
+			nodes(fromNode) = 1
+		}	
+	}
 }
+
+var maxValue = -1
+var maxNode = -1
+
+nodes.keys.foreach { node =>
+	if (nodes(node) > maxValue){
+		maxValue = nodes(node)
+		maxNode = nodes(node)
+	}
+}
+
+println(s"maxNode: $maxNode, with value: $maxValue")
