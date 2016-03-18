1b14aa369ca95e4933a92a085e1f71760991bde3
ryant26 <ryant26@gmail.com>
2016-03-17 20:47:18 -0600

    task 1 done

--- src/task1.scala
+++ src/task1.scala
@@ -1,4 +1,2 @@
 val text = sc.textFile("data_input")
-val result = text.map(line => (line.split("\t")(0), 1)).reduceByKey(_ + _).reduce((a,b) => if (a._2 < b._2) b else a)._1
-
-// I believe I selected the wrong neighbor
+val result = text.map(line => (line.split("\t")(1), 1)).reduceByKey(_ + _).reduce((a,b) => if (a._2 < b._2) b else a)._2
