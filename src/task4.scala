aed417d3e31f74650079c58ce6b9c6c213a5b3f9
jbcuglietta <jbcuglietta@gmail.com>
2016-04-05 22:41:26 -0600

    Update task4.scala

--- src/task4.scala
+++ src/task4.scala
@@ -2,7 +2,6 @@ val text = sc.textFile("data_input")
 var edges = text.map(line => (line.split("\t")(0), line.split("\t")(1)))
 var singlehop = edges.groupByKey()
 var isPointedTo = text.map(line => (line.split("\t")(1), line.split("\t")(0)))
-var isPointedToGroup = isPointedTo.groupByKey()
-var hopsTo = isPointedToGroup.join(singlehop)
+var hopsTo = isPointedTo.join(singlehop)
 var doublehop = hopsTo.map(x => (x._2._1,x._2._2))
 val result = singlehop union doublehop
