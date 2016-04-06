7891506fd314d8a107c173e8d23cd6eb57f01c51
jbcuglietta <jbcuglietta@gmail.com>
2016-04-05 23:42:51 -0600

    please be the last

--- src/task4.scala
+++ src/task4.scala
@@ -4,4 +4,4 @@ var isPointedTo = text.map(line => (line.split("\t")(1), line.split("\t")(0)))
 var hopsTo = isPointedTo.join(singlehop)
 var doublehop = hopsTo.map(x => (x._2._1,x._2._2))
 var unfilteredresult = singlehop union doublehop
-val result = unfilteredresult.sortByKey(true).filter(x=> x._1 != x._2)
+val result = unfilteredresult.sortByKey(true).filter(x=> x._1 != x._2).collect()
