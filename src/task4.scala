18fba6eba6ae95ee64f78eb8d39be357afd5afca
jbcuglietta <jbcuglietta@gmail.com>
2016-04-05 23:35:17 -0600

    Update task4.scala

--- src/task4.scala
+++ src/task4.scala
@@ -1,6 +1,7 @@
 val text = sc.textFile("data_input")
-var edges = text.map(line => (line.split("\t")(0), line.split("\t")(1)))
+var singlehop = text.map(line => (line.split("\t")(0), line.split("\t")(1)))
 var isPointedTo = text.map(line => (line.split("\t")(1), line.split("\t")(0)))
 var hopsTo = isPointedTo.join(singlehop)
 var doublehop = hopsTo.map(x => (x._2._1,x._2._2))
-val result = singlehop union doublehop
+var unfilteredresult = singlehop union doublehop
+val result = unfilteredresult.filter(x=> x._1 != x._2).sortByKey(true)
