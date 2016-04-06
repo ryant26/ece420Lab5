42720e8312f78092eecb3b8f2885951437d61748
jbcuglietta <jbcuglietta@gmail.com>
2016-04-05 22:26:18 -0600

    Task4 updated

--- src/task4.scala
+++ src/task4.scala
@@ -1,7 +1,8 @@
 val text = sc.textFile("data_input")
 var edges = text.map(line => (line.split("\t")(0), line.split("\t")(1)))
-var adjacency = edges.groupByKey()
-
-var comp = edges.flatmap { x=>
-	edges.filter(y => y.1)
-}
+var singlehop = edges.groupByKey()
+var isPointedTo = text.map(line => (line.split("\t")(1), line.split("\t")(0)))
+var isPointedToGroup = isPointedTo.groupByKey()
+var hopsTo = isPointedToGroup.join(singlehop)
+var doublehop = hopsTo.map(x => (x._2._1,x._2._2))
+val result = singlehop union doublehop
